8722ccbc9e9ccb40d0476ab2f3445973c5b1fa16
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-05 23:58:28 -0300

    Percorrendo a Lista na View

--- backend/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ backend/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,29 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 
+
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Xiaomi Redmi");
+		pedido.setUrlImagem("image.gpg");
+		pedido.setUrlProduto("http://amazon.com.br");
+		pedido.setDescricao("Descrição");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido, pedido, pedido);
+		model.addAttribute("pedidos", pedidos);
+		
 		return "home";
 	}
 }
