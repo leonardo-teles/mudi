bb8bdfe8dc2f27114c301c48e6610b8341aeea33
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-09 08:52:41 -0300

    Rotas do Status

--- backend/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ backend/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -1,11 +1,16 @@
 package br.com.alura.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.alura.mvc.mudi.enums.StatusPedido;
 import br.com.alura.mvc.mudi.model.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
 
 }
