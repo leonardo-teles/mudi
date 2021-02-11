aabca7334e5f9d9bc5de54c881da55e55c1d25ca
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-10 23:22:46 -0300

    Trabalhando com Paginação

--- backend/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ backend/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.alura.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +14,7 @@ import br.com.alura.mvc.mudi.model.Pedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-	List<Pedido> findByStatus(StatusPedido status);
+	List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
 	@Query("SELECT p FROM Pedido p JOIN p.usuario u WHERE u.username = :username")
 	List<Pedido> findAllByUsuario(@Param("username") String username);
